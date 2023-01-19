048db299c5d50f4a304f55b2434083937c04c30c
cnx <chennanxiao@guming.com>
2023-01-19 23:37:02 +0800

    feat: 借阅历史

--- src/pages/package/home/order-history/index.tsx
+++ src/pages/package/home/order-history/index.tsx
@@ -1,30 +1,114 @@
 import React, { useState, useCallback, useEffect } from "react";
-import { View } from "@tarojs/components";
+import { useReachBottom } from "@tarojs/taro";
+import { View, Navigator, Image, Text } from "@tarojs/components";
 import { getUID } from "@/utils/permission";
 import { getOrdersByUserId } from "@/servers/apis/order";
+import { PageStatusIndicator } from "@/template/page-status-indicator";
+import NoData from "@/components/no-data";
+import LoadMore from "@/components/load-more";
+import { ORDER_STATUS } from "@/utils/constant";
 
 type LoadingType = "loading" | "error" | "done";
+type LoadMoreStatusType = "hidding" | "loading" | "nomore";
 
 const OldHistory = () => {
   const [pageStatus, setPageStatus] = useState<LoadingType>("loading");
-  const [orders, setOrders] = useState([]);
+  const [loadMoreStatus, setLoadMoreStatus] =
+    useState<LoadMoreStatusType>("hidding");
+  const [orders, setOrders] = useState<any>([]);
 
   const _loadPage = useCallback(() => {
     setPageStatus("loading");
     getOrdersByUserId(getUID(), "history")
       .then((res) => {
-        console.log(res);
         setPageStatus("done");
-        setOrders(res.data.orders);
+        setOrders(res.orders);
       })
-      .catch(() => setPageStatus("error"));
-  }, []);
+      .catch(() => {
+        setPageStatus("error");
+      });
+  }, [setPageStatus, setOrders]);
+
+  useReachBottom(() => {
+    if (loadMoreStatus !== "hidding") return;
+    setLoadMoreStatus("loading");
+    getOrdersByUserId(getUID(), "history", orders.length)
+      .then((res) => {
+        setLoadMoreStatus(res.orders.length ? "hidding" : "nomore");
+        setOrders(orders.concat(res.orders));
+      })
+      .catch(() => setLoadMoreStatus("hidding"));
+  });
 
   useEffect(() => {
     _loadPage();
   }, []);
 
-  return <View>OldHistory</View>;
+  return (
+    <>
+      <PageStatusIndicator pageStatus={pageStatus} />
+      {pageStatus === "done" && (
+        <>
+          {orders.length ? (
+            <View className="weui-cells weui-cells_after-title">
+              {orders.map((item, index) => {
+                return (
+                  <Navigator
+                    key={item.id}
+                    url="./children/order-detail?id={{item.id}}"
+                    className="weui-cell ui-book-card"
+                    hoverClass="ui--active"
+                    id="{{item.id}}"
+                  >
+                    <Image
+                      className="ui-book-card__img"
+                      src={item.book.imgs.small}
+                      mode="widthFix"
+                    />
+                    <View className="ui-book-card__info font-gray-26">
+                      <View className="ui-book-card__info-title font-black-34">
+                        {item.book.title}
+                      </View>
+                      <View>
+                        作者:
+                        {item.book.author.map((item) => {
+                          return (
+                            <Text
+                              key={item}
+                              className="ui-book-card__info-name"
+                            >
+                              {item}
+                            </Text>
+                          );
+                        })}
+                      </View>
+                      <View>出版社: {item.book.publisher}</View>
+                      <View>图书馆: {item.library.name}</View>
+                      {item.status == ORDER_STATUS.CANCELED_BY_USER ? (
+                        <>
+                          <View>创建日期：{item.created_at}</View>
+                          <View>订单状态：已取消</View>
+                        </>
+                      ) : (
+                        <>
+                          <View>借阅日期：{item.actual_take_time}</View>
+                          <View>还书日期：{item.actual_return_time}</View>
+                        </>
+                      )}
+                    </View>
+                    <View className="weui-cell__ft_in-access"></View>
+                  </Navigator>
+                );
+              })}
+            </View>
+          ) : (
+            <NoData />
+          )}
+          <LoadMore status={loadMoreStatus} nomoreText="没有更多订单了" />
+        </>
+      )}
+    </>
+  );
 };
 
 export default React.memo(OldHistory);
--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -8,3 +8,16 @@
 export const USER_STATUS_REVIEWING = 0; // 审核中
 export const USER_STATUS_APPROVED = 1; // 审核通过
 export const USER_STATUS_REJECTED = 2; // 审核未通过
+
+/**
+ * 和业务相关的一些常量，具体定义见constant.js
+ */
+export const ORDER_STATUS = {
+  WAITING_FOR_OTHERS_TO_RETURN: 1001,
+  WAITING_TO_TAKE_RETURNED_BOOK: 1002,
+  WAITING_TO_TAKE_AT_PLANED_TIME: 1003,
+  BORROWING: 1004,
+  NORMAL_CLOSE: 1011,
+  ABNORMAL_CLOSE: 1012,
+  CANCELED_BY_USER: 1021,
+};
